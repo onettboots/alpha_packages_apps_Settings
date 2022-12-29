ReloadedOS/packages_apps_Settings

Settings: use a new task stack for app lock fragments
* fixes fragments staying in recents on going home
* also made other preferences do binder calls asynchronously

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/settings/security/applock/AppLockBiometricPreferenceController.kt
@@ -19,37 +19,57 @@ package com.android.settings.security.applock
 import android.app.AppLockManager
 import android.content.Context
 import android.hardware.biometrics.BiometricManager
-import android.hardware.biometrics.BiometricManager.Authenticators
+import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
 
 import androidx.preference.Preference
-import androidx.preference.SwitchPreference
+import androidx.preference.PreferenceScreen
 
-import com.android.settings.core.BasePreferenceController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "app_lock_biometrics_allowed"
 
 class AppLockBiometricPreferenceController(
     context: Context,
-    key: String,
-) : BasePreferenceController(context, key),
-        Preference.OnPreferenceChangeListener {
+    private val coroutineScope: CoroutineScope
+) : AppLockTogglePreferenceController(context, KEY) {
 
     private val appLockManager = context.getSystemService(AppLockManager::class.java)
     private val biometricManager = context.getSystemService(BiometricManager::class.java)
 
+    private var preference: Preference? = null
+    private var isBiometricsAllowed = false
+
+    init {
+        coroutineScope.launch {
+            isBiometricsAllowed = withContext(Dispatchers.Default) {
+                appLockManager.isBiometricsAllowed()
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
     override fun getAvailabilityStatus(): Int {
-        val biometricsAllowed = biometricManager.canAuthenticate(
-            Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
-        return if (biometricsAllowed)
-            AVAILABLE
-        else
-            UNSUPPORTED_ON_DEVICE
+        val result = biometricManager.canAuthenticate(BIOMETRIC_STRONG)
+        return if (result == BiometricManager.BIOMETRIC_SUCCESS) AVAILABLE else CONDITIONALLY_UNAVAILABLE
     }
 
-    override fun updateState(preference: Preference) {
-        (preference as SwitchPreference).setChecked(appLockManager.isBiometricsAllowed())
-    }
+    override fun isChecked() = isBiometricsAllowed
 
-    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
-        appLockManager.setBiometricsAllowed(newValue as Boolean)
+    override fun setChecked(checked: Boolean): Boolean {
+        isBiometricsAllowed = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setBiometricsAllowed(isBiometricsAllowed)
+        }
         return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
     }
 }

onettboots/alpha_packages_apps_Settings

Change-Id: Ie5330da9aa5039c3e5ed30ce47dcec8c01501e75

src/com/android/settings/security/applock/AppLockBiometricPreferenceController.kt
@@ -19,37 +19,59 @@ package com.android.settings.security.applock
 import android.app.AppLockManager
 import android.content.Context
 import android.hardware.biometrics.BiometricManager
-import android.hardware.biometrics.BiometricManager.Authenticators
+import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
 
 import androidx.preference.Preference
-import androidx.preference.SwitchPreference
+import androidx.preference.PreferenceScreen
 
-import com.android.settings.core.BasePreferenceController
+import com.android.settings.custom.CustomTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "app_lock_biometrics_allowed"
 
 class AppLockBiometricPreferenceController(
     context: Context,
-    key: String,
-) : BasePreferenceController(context, key),
-        Preference.OnPreferenceChangeListener {
+    private val coroutineScope: CoroutineScope
+) : CustomTogglePreferenceController(context, KEY) {
 
     private val appLockManager = context.getSystemService(AppLockManager::class.java)
     private val biometricManager = context.getSystemService(BiometricManager::class.java)
 
+    private var preference: Preference? = null
+    private var isBiometricsAllowed = false
+
+    init {
+        coroutineScope.launch {
+            isBiometricsAllowed = withContext(Dispatchers.Default) {
+                appLockManager.isBiometricsAllowed()
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
     override fun getAvailabilityStatus(): Int {
-        val biometricsAllowed = biometricManager.canAuthenticate(
-            Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
-        return if (biometricsAllowed)
-            AVAILABLE
-        else
-            UNSUPPORTED_ON_DEVICE
+        val result = biometricManager.canAuthenticate(BIOMETRIC_STRONG)
+        return if (result == BiometricManager.BIOMETRIC_SUCCESS) AVAILABLE else CONDITIONALLY_UNAVAILABLE
     }
 
-    override fun updateState(preference: Preference) {
-        (preference as SwitchPreference).setChecked(appLockManager.isBiometricsAllowed())
-    }
+    override fun isChecked() = isBiometricsAllowed
 
-    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
-        appLockManager.setBiometricsAllowed(newValue as Boolean)
+    override fun setChecked(checked: Boolean): Boolean {
+        isBiometricsAllowed = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setBiometricsAllowed(isBiometricsAllowed)
+        }
         return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
     }
 }