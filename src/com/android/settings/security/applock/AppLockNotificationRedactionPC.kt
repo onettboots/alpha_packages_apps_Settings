Totozao/packages_apps_FlamingoSettings

applock: adapt for API changes
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

src/com/flamingo/settings/security/applock/AppLockNotificationRedactionPC.kt
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flamingo.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.flamingo.settings.FlamingoTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "redact_notifications"
+
+class AppLockNotificationRedactionPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : FlamingoTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var shouldRedactNotification = false
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            shouldRedactNotification = withContext(Dispatchers.Default) {
+                appLockManager.packageData.find {
+                    it.packageName == packageName
+                }?.shouldRedactNotification == true
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = shouldRedactNotification
+
+    override fun setChecked(checked: Boolean): Boolean {
+        shouldRedactNotification = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setShouldRedactNotification(packageName, checked)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen);
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/flamingo/settings/security/applock/AppLockPackageConfigFragment.kt
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flamingo.settings.security.applock
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+
+import androidx.lifecycle.lifecycleScope
+
+import com.android.settings.R
+import com.android.settings.widget.EntityHeaderController
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.widget.LayoutPreference
+import com.flamingo.settings.FlamingoDashboardFragment
+
+private val TAG = AppLockPackageConfigFragment::class.simpleName
+private const val KEY_HEADER = "header_view"
+
+class AppLockPackageConfigFragment : FlamingoDashboardFragment() {
+
+    private lateinit var pm: PackageManager
+    private lateinit var packageInfo: PackageInfo
+
+    override fun onAttach(context: Context) {
+        pm = context.packageManager
+        packageInfo = arguments?.getParcelable(PACKAGE_INFO, PackageInfo::class.java)!!
+        super.onAttach(context)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        val header = preferenceScreen.findPreference<LayoutPreference>(KEY_HEADER)
+        EntityHeaderController.newInstance(
+            requireActivity(),
+            this,
+            header?.findViewById(R.id.entity_header)
+        ).setRecyclerView(listView, settingsLifecycle)
+            .setPackageName(packageInfo.packageName)
+            .setButtonActions(
+                EntityHeaderController.ActionType.ACTION_NONE,
+                EntityHeaderController.ActionType.ACTION_NONE
+            )
+            .bindHeaderButtons()
+            .setLabel(getLabel(packageInfo))
+            .setIcon(getIcon(packageInfo))
+            .done(requireActivity(), false /* rebindActions */)
+    }
+
+    private fun getLabel(packageInfo: PackageInfo) =
+        packageInfo.applicationInfo.loadLabel(pm).toString()
+
+    private fun getIcon(packageInfo: PackageInfo) =
+        packageInfo.applicationInfo.loadIcon(pm)
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockPackageProtectionPC(context, packageInfo.packageName, lifecycleScope),
+        AppLockNotificationRedactionPC(context, packageInfo.packageName, lifecycleScope),
+    )
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_config_settings
+
+    override protected fun getLogTag() = TAG
+}

onettboots/alpha_packages_apps_Settings

Change-Id: I5daea6757c2116f9eaf3fca3ee11f963d595ec4c

src/com/android/settings/security/applock/AppLockNotificationRedactionPC.kt
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.settings.custom.CustomTogglePreferenceController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val KEY = "redact_notifications"
+
+class AppLockNotificationRedactionPC(
+    context: Context,
+    private val packageName: String,
+    private val coroutineScope: CoroutineScope
+) : CustomTogglePreferenceController(context, KEY) {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+    private var shouldRedactNotification = false
+    private var preference: Preference? = null
+
+    init {
+        coroutineScope.launch {
+            shouldRedactNotification = withContext(Dispatchers.Default) {
+                appLockManager.packageData.find {
+                    it.packageName == packageName
+                }?.shouldRedactNotification == true
+            }
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun isChecked() = shouldRedactNotification
+
+    override fun setChecked(checked: Boolean): Boolean {
+        shouldRedactNotification = checked
+        coroutineScope.launch(Dispatchers.Default) {
+            appLockManager.setShouldRedactNotification(packageName, checked)
+        }
+        return true
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen);
+        preference = screen.findPreference(preferenceKey)
+    }
+}

src/com/android/settings/security/applock/AppLockPackageConfigFragment.kt
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+
+import androidx.lifecycle.lifecycleScope
+
+import com.android.settings.R
+import com.android.settings.widget.EntityHeaderController
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.widget.LayoutPreference
+import com.android.settings.CustomDashboardFragment
+
+private val TAG = AppLockPackageConfigFragment::class.simpleName
+private const val KEY_HEADER = "header_view"
+
+class AppLockPackageConfigFragment : CustomDashboardFragment() {
+
+    private lateinit var pm: PackageManager
+    private lateinit var packageInfo: PackageInfo
+
+    override fun onAttach(context: Context) {
+        pm = context.packageManager
+        packageInfo = arguments?.getParcelable(PACKAGE_INFO, PackageInfo::class.java)!!
+        super.onAttach(context)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        val header = preferenceScreen.findPreference<LayoutPreference>(KEY_HEADER)
+        EntityHeaderController.newInstance(
+            requireActivity(),
+            this,
+            header?.findViewById(R.id.entity_header)
+        ).setRecyclerView(listView, settingsLifecycle)
+            .setPackageName(packageInfo.packageName)
+            .setButtonActions(
+                EntityHeaderController.ActionType.ACTION_NONE,
+                EntityHeaderController.ActionType.ACTION_NONE
+            )
+            .bindHeaderButtons()
+            .setLabel(getLabel(packageInfo))
+            .setIcon(getIcon(packageInfo))
+            .done(requireActivity(), false /* rebindActions */)
+    }
+
+    private fun getLabel(packageInfo: PackageInfo) =
+        packageInfo.applicationInfo.loadLabel(pm).toString()
+
+    private fun getIcon(packageInfo: PackageInfo) =
+        packageInfo.applicationInfo.loadIcon(pm)
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockPackageProtectionPC(context, packageInfo.packageName, lifecycleScope),
+        AppLockNotificationRedactionPC(context, packageInfo.packageName, lifecycleScope),
+    )
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_config_settings
+
+    override protected fun getLogTag() = TAG
+}